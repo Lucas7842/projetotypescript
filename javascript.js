d22b1d6d40680b4de890c6e653da6023ff8dffad
Lucas Martins <lucasmartins1096@hotmail.com>
2024-06-30 12:53:27 -0300

    Atualização

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 const input = document.querySelector('input');
 
 function totalMudou(){
+  const value = Number(input.value);
   const p = document.querySelector('p');
-  p.innerText = `ganho toatal: ${input.value}`;
+  p.innerText = `ganho toatal: ${value + 100 - value * 0.2}`;
 }
 
 input.addEventListener('keyup', totalMudou);
